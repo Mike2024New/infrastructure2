6f5aebf58ebaf184d61f2993ee369393ddf8e137
Mike2024New <proggerman2022@yandex.ru>
2026-07-30 13:58:59 +0300

    f0b106

--- packages/infrastructure_message_bus/src/infrastructure_message_bus/__init__.py
+++ packages/infrastructure_message_bus/src/infrastructure_message_bus/__init__.py
@@ -1,10 +1,7 @@
 from infrastructure_message_bus.main import MessagePrintSettings, FileLogSettings
 from infrastructure_message_bus.factory import message_bus_factory
 
-temp_variable = 1000
-
 __all__ = [
-    'temp_variable',
     'MessagePrintSettings', 'FileLogSettings',  # типы настроек
     'message_bus_factory',  # фабрика
 ]
