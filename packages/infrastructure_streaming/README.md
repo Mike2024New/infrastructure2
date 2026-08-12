f470629e82adbb464fd66f6f520d1c7cb3542d1c
Mike2024New <proggerman2022@yandex.ru>
2026-08-12 12:41:01 +0300

    11d712

---
+++ packages/infrastructure_streaming/README.md
@@ -0,0 +1,7 @@
+# Streaming - набор стриминговых клиентов
+
+### url для установки пакета (прописать в pyproject.toml целевого проекта)
+
+```text
+"infrastructure-streaming @ git+https://github.com/Mike2024New/infrastructure2.git@main#subdirectory=packages/infrastructure_streaming"
+```
---
+++ packages/infrastructure_streaming/src/infrastructure_streaming/consumer.py
@@ -0,0 +1,42 @@
+import asyncio, json, websockets
+from typing import Callable, Any
+
+
+async def consume_stream(
+        url: str,
+        data: str,
+        callback: Callable[[Any], None],
+        event: asyncio.Event,
+        timeout: float = 1.0
+) -> None:
+    """
+    Потребитель стрима. Отправляет запрос и получает ответы частями.
+    Паттерн 'данные передаются на сервер' -> 'сервер выдает ответ порциями' например ответ от llm, callback обрабатывает
+    полученные чанки (порции)
+    :param url: url формата websocket, например ws://{host}:{port}/ws
+    :param data: передаваемые на сервер данные
+    :param callback: функция обрабатывающая ответ сервера, json ответ
+    :param event: прерыватель стрима (установить set во внешнем коде и стрим остановится досрочно)
+    :param timeout: ожидание ответа сервера время в секундах
+    :return: None
+    """
+    async with websockets.connect(url) as ws:
+        try:
+            await ws.send(data)
+            while True:
+                if event.is_set():
+                    print(f'stream cancelled')
+                    break
+                response = await asyncio.wait_for(ws.recv(), timeout=timeout)
+                response = json.loads(response)
+                try:
+                    callback(response)
+                except Exception as err:
+                    raise RuntimeError(f'Callback error: {err}')
+        except asyncio.TimeoutError:
+            raise
+        except websockets.exceptions.ConnectionClosedError:
+            pass  # соединение разорвано, обрабатывать через callback
+        except Exception as err:
+            print(f"stream error: {err}")
+            raise
